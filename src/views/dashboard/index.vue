3b1666c6ad2bf8de423488a4a847dba6ac977962
郑文林 <zwl_0503@163.com>
2019-08-22 16:26:06 +0800

    first-commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard-container">
+     <el-card>
+         <p>UI组件库</p>
+          <hr>
+         <div>
+            分为两部分实现：<br>
+            1.整体布局：
+            <ul>
+              <li>全局固定路由和异步路由结合</li>
+              <li>根据路由表动态生成左侧菜单栏</li>
+              <li>用户角色和路由匹配实现权限控制</li>
+              <li>全局皮肤设置</li>
+              <li>全局按钮设置</li>
+              <li>全局字体大小设置</li>
+            </ul>
+            2.组件部分：
+            <ul>
+              <li>table部分：可搜索table，动态增加删除行table，树形table，自定义单元格</li>
+              <li>表单：下拉，单选 ，多选，文本域等</li>
+              <li>icons</li>
+              <li>charts</li>
+              <li>树形控件</li>
+              <li>可拓展部分</li>
+            </ul>
+         </div>
+     </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
---
+++ src/views/select-tree/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="select-tree">
+      <p>多选：</p>
+      <treeselect v-model="value"
+                  :flat="true" 
+                  multiple="multiple" 
+                  placeholder="请选择"
+                  :options="options" />
+
+      <p class="p">单选：</p>
+       <treeselect v-model="value1" 
+                  :multiple="false" 
+                  placeholder="请选择"
+                  :options="options" />
+  </div>
+</template>
+
+<script>
+// import the component
+  import Treeselect from '@riophae/vue-treeselect'
+  // import the styles
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
+export default {
+ components: { Treeselect },
+  data() {
+      return {
+        // define the default value
+        value: null,
+        value1:null,
+        // define options
+        options: [ {
+          id: 'a',
+          label: 'a',
+          children: [ {
+            id: 'aa',
+            label: 'aa',
+          }, {
+            id: 'ab',
+            label: 'ab',
+          } ],
+        }, {
+          id: 'b',
+          label: 'b',
+        }, {
+          id: 'c',
+          label: 'c',
+        } ],
+      }
+    },
+};
+</script>
+<style lang="scss">
+ .select-tree{
+     padding: 15px;
+     margin-top: 15px;
+     .p{
+         border-top: 1px solid #eef;
+         padding-top: 20px;
+         margin-top: 60px;
+     }
+ }
+</style>
